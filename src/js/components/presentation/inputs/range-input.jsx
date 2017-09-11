f32f395421ea7f4ffac5696f286423b756f1b553
Jake Cooley <jake@ideasbynature.com>
2017-09-11 23:11:58 +0700

    hide label instead of remove

--- src/js/components/presentation/inputs/range-input.jsx
+++ src/js/components/presentation/inputs/range-input.jsx
@@ -6,7 +6,7 @@ import 'styles/components/inputs'
 const RangeInput = (props) => {
     let {
         className, errors, formErrors,
-        id, label, placeholder, showLabel,
+        id, label, placeholder, showLabel, labelClass,
         required, validateAs, value, ...rest
     } = props;
 
@@ -16,16 +16,18 @@ const RangeInput = (props) => {
         attr['required'] = true;
         attr['aria-required'] = true;
     }
+    let labelTextClasses = `input__label__text ${ labelClass ? labelClass : '' } ${ showLabel ? '' : ' u-sr-only' }`;
 
     return (
-        <label className='input__label input__label--wrap'>
-            { label && showLabel }{ attr.required &&
-        <span>{ '\u00A0' }*<span className="u-sr-only"> required field</span></span> }
-          <input type='range' name={ id } id={ id } onChange={ props.onChange } min='0' max={ props.total }
-                 className={ `input input--range ${ className ? className : '' } ${ formErrors && formErrors[id] ? 'input--invalid' : '' }` }
-                 data-validate={ validateAs } placeholder={ placeholder } value={ value } step='0.01'
-                 onMouseOut={ props.onBlur }
-                 { ...attr } />
+        <label className="input__label input__label--wrap">
+          <span className={ labelTextClasses }>
+            { label }{ attr.required && <span>{ '\u00A0' }*<span className="u-sr-only"> required field</span></span> }
+          </span>
+            <input type='range' name={ id } id={ id } onChange={ props.onChange } min='0' max={ props.total }
+                   className={ `input input--range ${ className ? className : '' } ${ formErrors && formErrors[id] ? 'input--invalid' : '' }` }
+                   data-validate={ validateAs } placeholder={ placeholder } value={ value } step='0.01'
+                   onMouseOut={ props.onBlur }
+                   { ...attr } />
             { formErrors && formErrors[id] ?
                 <div className='input__error'>{ errors }</div>
                 :
@@ -38,6 +40,7 @@ RangeInput.propTypes = {
     className: PropTypes.string,
     errors: PropTypes.string,
     id: PropTypes.string.isRequired,
+    labelClass: PropTypes.string,
     label: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.element
