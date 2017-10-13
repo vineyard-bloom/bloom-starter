1263758f9bfad0ea003c9e30cb1be58c02c1ea6e
Katie Foster <fosterkatiea@gmail.com>
2017-10-13 15:05:08 -0600

    removing console log

--- src/js/presentation/forms/example-form.jsx
+++ src/js/presentation/forms/example-form.jsx
@@ -18,8 +18,6 @@ const ExampleForm = (props) => {
     { label: 'Cakes', value: 'birthday cakes' }
   ]
 
-  console.log('hi')
-
   const toggleClick = (e) => {
     e.preventDefault();
     props.manualFieldUpdate(props.formId, 'toggle', formData && formData.toggle && !formData.toggle.value)
