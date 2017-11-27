4040fa978e985de9e4be44548364b84034eb676a
Katie Foster <fosterkatiea@gmail.com>
2017-11-27 16:08:17 -0700

    aria and accessibility labels for modal

--- src/js/presentation/layout/modal.jsx
+++ src/js/presentation/layout/modal.jsx
@@ -143,13 +143,13 @@ class Modal extends React.Component {
 
     return (
       <div className={ `Modal ${ modalContents ? 'is-active' : 'is-hidden' }` } onKeyDown={ this.keyDownHandler } id='modal-wrapper'
-        onClick={ this.detectClickOff } onBlur={ this.onFocusOut } tabIndex={ modalContents ? 0 : -1 }>
+        onClick={ this.detectClickOff } onBlur={ this.onFocusOut } tabIndex={ modalContents ? 0 : -1 } aria-live='polite' role='dialog'>
         <Transition in={ !!modalContents } timeout={ 0 }>
             {(status) =>
-              <div className={ `Modal-content descend-${status}` }>
-                <button className='Btn--null Btn-close' id='modal-close-button'
+              <div className={ `Modal-content descend-${status}` } aria-hidden={ !modalContents }>
+                <button className='Btn--null Btn-close' id='modal-close-button' aria-label='close this modal'
                   onClick={ (e) => { e.preventDefault(); props.closeModal() } } tabIndex={ modalContents ? 0 : -1 }>
-                  x
+                  <SVGInline svg={ close } />
                 </button>
                 { modalContents }
               </div>
