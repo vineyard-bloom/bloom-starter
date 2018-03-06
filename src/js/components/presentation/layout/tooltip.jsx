464be84b1164b302b5b5af8599a9dbd6244df72a
Katie Foster <fosterkatiea@gmail.com>
2018-03-06 09:37:23 -0700

    moving bloom override styles into their own folder; importing tooltip into errortip properly

--- src/js/components/presentation/layout/tooltip.jsx
+++ src/js/components/presentation/layout/tooltip.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import 'styles/components/tooltip.scss'
-import 'styles/components/errortip.scss'
 
 class Tooltip extends React.Component {
   state = {
