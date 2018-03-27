7325a20157fe1fe5ced87a25cf1819acea461786
Katie Foster <fosterkatiea@gmail.com>
2018-03-27 12:17:08 -0600

    little syntactic things

--- scripts/cross-os-prettier.js
+++ scripts/cross-os-prettier.js
@@ -1,28 +1,28 @@
 #! /usr/bin/env node
-var path = require('path');
-var execSync = require('child_process').execSync;
-var SEPARATOR = process.platform === 'win32' ? ';' : ':';
-var env = Object.assign({}, process.env);
+var path = require('path')
+var execSync = require('child_process').execSync
+var SEPARATOR = process.platform === 'win32' ? ';' : ':'
+var env = Object.assign({}, process.env)
 
-env.PATH = path.resolve('./node_modules/.bin') + SEPARATOR + env.PATH;
+env.PATH = path.resolve('./node_modules/.bin') + SEPARATOR + env.PATH
 
 function myExecSync(cmd) {
   var output = execSync(cmd, {
     cwd: process.cwd(),
     env: env
-  });
+  })
 
-  console.log('\x1b[37m', output.toString('utf8'));
+  console.log('\x1b[37m', output.toString('utf8'))
 }
 
 if (process.platform === 'win32') {
-  console.log('\x1b[32m', 'running prettier on windows platform');
+  console.log('\x1b[32m', 'running prettier on windows platform')
   return myExecSync(
     'prettier --config .prettierrc --write **/*.{js,jsx,scss}'
-  );
+  )
 } else {
-  console.log('\x1b[32m', 'running prettier on non-windows platform');
+  console.log('\x1b[32m', 'running prettier on non-windows platform')
   var prettierScript =
-    "./node_modules/.bin/prettier --config .prettierrc --write '**/*.{js,jsx,scss}'";
-  return myExecSync(prettierScript);
+    "./node_modules/.bin/prettier --config .prettierrc --write '**/*.{js,jsx,scss}'"
+  return myExecSync(prettierScript)
 }
