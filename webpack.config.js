f3470db7253fb54ef1feb814b0d08c06d51555ff
Katie Foster <fosterkatiea@gmail.com>
2018-03-27 11:06:30 -0600

    removing script in favor of cross-platform .prettierignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const autoprefixer = require('autoprefixer')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-const webpack = require('webpack')
+const autoprefixer = require('autoprefixer');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
-const APP_DIR = path.join(__dirname, '/src/')
-const BUILD_DIR = path.join(__dirname, '/dist/')
-const config = require(path.join(__dirname, '/config/config.json'))
+const APP_DIR = path.join(__dirname, '/src/');
+const BUILD_DIR = path.join(__dirname, '/dist/');
+const config = require(path.join(__dirname, '/config/config.json'));
 
 const baseConfig = {
   entry: {
@@ -29,18 +29,14 @@ const baseConfig = {
           path.join(__dirname, 'src/images'),
           path.join(__dirname, 'public/fonts')
         ],
-        exclude: [
-          path.join(__dirname, 'src/images/inline-svgs')
-        ],
+        exclude: [path.join(__dirname, 'src/images/inline-svgs')],
         options: {
           limit: 10000
         }
       },
       {
         test: /\.svg$/,
-        include: [
-          path.join(__dirname, 'src/images/inline-svgs')
-        ],
+        include: [path.join(__dirname, 'src/images/inline-svgs')],
         loader: 'raw-loader'
       },
       {
@@ -49,7 +45,7 @@ const baseConfig = {
         exclude: /node_modules/,
         query: {
           presets: ['react', 'env'],
-          'plugins': [
+          plugins: [
             'transform-object-rest-spread',
             'transform-class-properties'
           ]
@@ -60,10 +56,11 @@ const baseConfig = {
         use: [
           { loader: 'style-loader' },
           { loader: 'css-loader' },
-          { loader: 'postcss-loader',
+          {
+            loader: 'postcss-loader',
             options: {
               plugins: function() {
-                return [autoprefixer]
+                return [autoprefixer];
               }
             }
           },
@@ -81,44 +78,46 @@ const baseConfig = {
     }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      minChunks: ({ resource }) => /node_modules/.test(resource),
+      minChunks: ({ resource }) => /node_modules/.test(resource)
     }),
     new webpack.optimize.CommonsChunkPlugin('manifest'),
-    new CopyWebpackPlugin([
-      { from: 'public/index.html' }
-    ]),
+    new CopyWebpackPlugin([{ from: 'public/index.html' }]),
     new HtmlWebpackPlugin({ template: './public/index.html' })
   ],
 
   resolve: {
     alias: {
-      config:       path.resolve(__dirname, 'config'),
-      components:   path.resolve(__dirname, 'src/js/components'),
-      helpers:      path.resolve(__dirname, 'src/js/util/helpers'),
-      icons:        path.resolve(__dirname, 'src/js/icons'),
-      images:       path.resolve(__dirname, 'src/images'),
-      js:           path.resolve(__dirname, 'src/js'),
-      language:     path.resolve(__dirname, 'config/en.json'),
-      layout:       path.resolve(__dirname, 'src/js/components/presentation/layout'),
-      lib:          path.resolve(__dirname, 'lib'),
-      pages:        path.resolve(__dirname, 'src/js/pages'),
+      config: path.resolve(__dirname, 'config'),
+      components: path.resolve(__dirname, 'src/js/components'),
+      helpers: path.resolve(__dirname, 'src/js/util/helpers'),
+      icons: path.resolve(__dirname, 'src/js/icons'),
+      images: path.resolve(__dirname, 'src/images'),
+      js: path.resolve(__dirname, 'src/js'),
+      language: path.resolve(__dirname, 'config/en.json'),
+      layout: path.resolve(__dirname, 'src/js/components/presentation/layout'),
+      lib: path.resolve(__dirname, 'lib'),
+      pages: path.resolve(__dirname, 'src/js/pages'),
       presentation: path.resolve(__dirname, 'src/js/components/presentation'),
-      'redux-store':path.resolve(__dirname, 'src/js/redux-store'),
-      requests:     path.resolve(__dirname, 'src/js/util/requests'),
-      routes:       path.resolve(__dirname, 'src/js/routes'),
-      services:     path.resolve(__dirname, 'src/js/services'),
-      stubs:        path.resolve(__dirname, 'src/js/stubs'),
-      styles:       path.resolve(__dirname, 'src/styles'),
-      types:        path.resolve(__dirname, 'src/js/util/types'),
-      util:         path.resolve(__dirname, 'src/js/util'),
-      validator:    path.resolve(__dirname, 'src/js/util/validator')
+      'redux-store': path.resolve(__dirname, 'src/js/redux-store'),
+      requests: path.resolve(__dirname, 'src/js/util/requests'),
+      routes: path.resolve(__dirname, 'src/js/routes'),
+      services: path.resolve(__dirname, 'src/js/services'),
+      stubs: path.resolve(__dirname, 'src/js/stubs'),
+      styles: path.resolve(__dirname, 'src/styles'),
+      types: path.resolve(__dirname, 'src/js/util/types'),
+      util: path.resolve(__dirname, 'src/js/util'),
+      validator: path.resolve(__dirname, 'src/js/util/validator')
     },
     extensions: ['.jsx', '.js', '.html', '.scss']
   }
-}
+};
 
-if (config.app && config.app.environment && (config.app.environment === 'development')) {
-  baseConfig.devtool = 'eval-source-map'
+if (
+  config.app &&
+  config.app.environment &&
+  config.app.environment === 'development'
+) {
+  baseConfig.devtool = 'eval-source-map';
   baseConfig.devServer = {
     publicPath: '/',
     contentBase: './public',
@@ -127,49 +126,59 @@ if (config.app && config.app.environment && (config.app.environment === 'develop
     open: false,
     historyApiFallback: true,
     disableHostCheck: true
-  }
+  };
   baseConfig.plugins = [
     new HtmlWebpackExternalsPlugin({
-        externals: [
-          {
-            module: 'butter',
-            entry: 'https://cdnjs.buttercms.com/buttercms-1.1.0.min.js',
-            global: 'Butter'
-          }
-        ]
-      })
-    ].concat(baseConfig.plugins)
-} else if (config.app && config.app.environment && (config.app.environment === 'production')) {
+      externals: [
+        {
+          module: 'butter',
+          entry: 'https://cdnjs.buttercms.com/buttercms-1.1.0.min.js',
+          global: 'Butter'
+        }
+      ]
+    })
+  ].concat(baseConfig.plugins);
+} else if (
+  config.app &&
+  config.app.environment &&
+  config.app.environment === 'production'
+) {
   baseConfig.plugins = [
     new HtmlWebpackExternalsPlugin({
-        externals: [
-          {
-            module: 'react',
-            entry: 'https://cdnjs.cloudflare.com/ajax/libs/react/16.2.0/umd/react.production.min.js',
-            global: 'React'
-          },
-          {
-            module: 'react-dom',
-            entry: 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.2.0/umd/react-dom.production.min.js',
-            global: 'ReactDOM'
-          },
-          {
-            module: 'moment',
-            entry: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js',
-            global: 'moment'
-          },
-          {
-            module: 'butter',
-            entry: 'https://cdnjs.buttercms.com/buttercms-1.1.0.min.js',
-            global: 'Butter'
-          }
-        ]
-      })
-    ].concat(baseConfig.plugins).concat([
-      new webpack.optimize.UglifyJsPlugin({ minimize: true })
-    ])
+      externals: [
+        {
+          module: 'react',
+          entry:
+            'https://cdnjs.cloudflare.com/ajax/libs/react/16.2.0/umd/react.production.min.js',
+          global: 'React'
+        },
+        {
+          module: 'react-dom',
+          entry:
+            'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.2.0/umd/react-dom.production.min.js',
+          global: 'ReactDOM'
+        },
+        {
+          module: 'moment',
+          entry:
+            'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js',
+          global: 'moment'
+        },
+        {
+          module: 'butter',
+          entry: 'https://cdnjs.buttercms.com/buttercms-1.1.0.min.js',
+          global: 'Butter'
+        }
+      ]
+    })
+  ]
+    .concat(baseConfig.plugins)
+    .concat([new webpack.optimize.UglifyJsPlugin({ minimize: true })]);
 } else {
-  console.log('%s\n\nNo Config Environment Set. Please edit config.json and restart.\n\n', 'color: red')
+  console.log(
+    '%s\n\nNo Config Environment Set. Please edit config.json and restart.\n\n',
+    'color: red'
+  );
 }
 
-module.exports = baseConfig
+module.exports = baseConfig;
